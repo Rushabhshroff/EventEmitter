ddf466b428bc842731b017dddbf893994ef2c36b
Rushabh Shroff <rushabhshroff@cryvis.com>
2020-05-20 10:39:48 +0530

    added throwing exception if not handled

--- EventEmitter.cs
+++ EventEmitter.cs
@@ -1,53 +1,97 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using System;
-public class EventEmitter<L,T>
+namespace CryvisNet
 {
-    private Dictionary<L, List<Action<T>>> handlers = new Dictionary<L, List<Action<T>>>();
-    public EventEmitter()
+    public class EventEmitter<L, T>
     {
-        handlers = new Dictionary<L, List<Action<T>>>();
-    }
-    public void On(L ev, Action<T> callback)
-    {
-        if (!handlers.ContainsKey(ev))
+        private Dictionary<L, List<Action<T>>> handlers = new Dictionary<L, List<Action<T>>>();
+        private Dictionary<L, List<Action<T>>> onceHandlers = new Dictionary<L, List<Action<T>>>();
+        public EventEmitter()
         {
-            handlers[ev] = new List<Action<T>>();
+            handlers = new Dictionary<L, List<Action<T>>>();
         }
-        handlers[ev].Add(callback);
-    }
-    public void Off(L ev, Action<T> callback)
-    {
-        if (!handlers.ContainsKey(ev))
+        public void on(L ev, Action<T> callback)
         {
-            return;
-        }
-
-        List<Action<T>> l = handlers[ev];
-        if (!l.Contains(callback))
-        {
-            return;
-        }
-
-        l.Remove(callback);
-        if (l.Count == 0)
-        {
-            handlers.Remove(ev);
-        }
-    }
-    public void emit(L name, T data)
-    {
-        if (!handlers.ContainsKey(name)) { return; }
-        foreach (Action<T> handler in this.handlers[name])
-        {
-            try
+            if (!handlers.ContainsKey(ev))
             {
-                handler(data);
+                handlers[ev] = new List<Action<T>>();
             }
-            catch (Exception ex)
+            handlers[ev].Add(callback);
+        }
+        public void once(L ev, Action<T> callback)
+        {
+            if (!onceHandlers.ContainsKey(ev))
             {
-                throw ex;
+                onceHandlers[ev] = new List<Action<T>>();
             }
+            onceHandlers[ev].Add(callback);
+        }
+        public void off(L ev, Action<T> callback)
+        {
+            if (!handlers.ContainsKey(ev))
+            {
+                return;
+            }
+
+            List<Action<T>> l = handlers[ev];
+            if (!l.Contains(callback))
+            {
+                return;
+            }
+
+            l.Remove(callback);
+            if (l.Count == 0)
+            {
+                handlers.Remove(ev);
+            }
+        }
+        public void emit(L name, T data)
+        {
+            if (!handlers.ContainsKey(name))
+            {
+                if (name.GetType() == typeof(Exception))
+                {
+                    throw name as Exception;
+                }
+                return;
+            }
+            foreach (Action<T> handler in this.handlers[name])
+            {
+                try
+                {
+                    handler(data);
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }
+            if (!onceHandlers.ContainsKey(name))
+            {
+                if (name.GetType() == typeof(Exception))
+                {
+                    throw name as Exception;
+                }
+                return;
+            }
+            foreach (Action<T> onceHandler in this.onceHandlers[name])
+            {
+                try
+                {
+                    onceHandler(data);
+                    this.onceHandlers.Remove(name);
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }
+        }
+        public void removeAllListeners()
+        {
+            handlers = new Dictionary<L, List<Action<T>>>();
+            onceHandlers = new Dictionary<L, List<Action<T>>>();
         }
     }
 }
